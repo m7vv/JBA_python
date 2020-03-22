2c5d2c9461bad9a588c63d4a083fc1ede3cd8c05
mvv <61985913+m7vv@users.noreply.github.com>
2020-03-22 10:14:30 +0200

    stage 2/5

--- Rock-Paper-Scissors/game.py
+++ Rock-Paper-Scissors/game.py
@@ -1,3 +1,11 @@
+import random
 beats = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
+state = [i for i in beats.keys()]
 user_choose = input()
-print(f'Sorry, but computer chose {beats[user_choose]}')
+comp_choose = random.choice(state)
+if user_choose == comp_choose:
+    print(f'There is a draw ({user_choose})')
+elif comp_choose == beats[user_choose]:
+    print(f'Sorry, but computer chose {comp_choose}')
+else:
+    print(f'Well done. Computer chose {comp_choose} and failed')
