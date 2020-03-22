4915cd76e9d3c2e6f362db9e2df6ca2107d8832f
mvv <61985913+m7vv@users.noreply.github.com>
2020-03-22 11:42:59 +0200

    Stage 3/5/ Endless game

--- Rock-Paper-Scissors/game.py
+++ Rock-Paper-Scissors/game.py
@@ -1,11 +1,18 @@
 import random
 beats = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
 state = [i for i in beats.keys()]
-user_choose = input()
-comp_choose = random.choice(state)
-if user_choose == comp_choose:
-    print(f'There is a draw ({user_choose})')
-elif comp_choose == beats[user_choose]:
-    print(f'Sorry, but computer chose {comp_choose}')
-else:
-    print(f'Well done. Computer chose {comp_choose} and failed')
+while True:
+    user_choose = input()
+    if user_choose == '!exit':
+        break
+    if user_choose not in state:
+        print('Invalid input')
+        continue
+    comp_choose = random.choice(state)
+    if user_choose == comp_choose:
+        print(f'There is a draw ({user_choose})')
+    elif comp_choose == beats[user_choose]:
+        print(f'Sorry, but computer chose {comp_choose}')
+    else:
+        print(f'Well done. Computer chose {comp_choose} and failed')
+print('Bye!')
