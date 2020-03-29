055c59a84909716acc81d0971715a11e8c4c936d
mvv <61985913+m7vv@users.noreply.github.com>
2020-03-30 00:52:53 +0300

    project completed

--- Rock-Paper-Scissors/game.py
+++ Rock-Paper-Scissors/game.py
@@ -1,6 +1,7 @@
 import random
-beats = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
-state = [i for i in beats.keys()]
+
+# beats = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
+# state = [i for i in beats.keys()]
 name = input('Enter your name:')
 print(f'Hello, {name}')
 holder = open('rating.txt', 'r')
@@ -9,6 +10,17 @@ for line in holder:
     name_f, score_f = line.split()
     if name == name_f:
         score = int(score_f)
+holder.close()
+from_user = input()
+if from_user == '':
+    from_user = 'scissors,rock,paper'
+state = from_user.split(',')
+n = len(state) - 1  # number of options
+beats = {}
+for i, item in enumerate(state):
+    options = state[i + 1:] + state[:i]
+    beats[item] = options[:n // 2]
+print("Okay, let's start")
 while True:
     user_choose = input()
     if user_choose == '!exit':
@@ -23,7 +35,7 @@ while True:
     if user_choose == comp_choose:
         print(f'There is a draw ({user_choose})')
         score += 50
-    elif comp_choose == beats[user_choose]:
+    elif comp_choose in beats[user_choose]:
         print(f'Sorry, but computer chose {comp_choose}')
     else:
         print(f'Well done. Computer chose {comp_choose} and failed')
