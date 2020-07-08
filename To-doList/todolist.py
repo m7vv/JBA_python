167230c8690b884b513cd44fe80fb10f78b14bcd
mvv <61985913+m7vv@users.noreply.github.com>
2020-07-08 22:16:12 +0300

    step 2 completed

--- To-doList/todolist.py
+++ To-doList/todolist.py
@@ -1,27 +1,57 @@
-# Write your code here
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class Table(Base):
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True)
+    task = Column(String, default='')
+    deadline = Column(Date, default=datetime.today())
+
+    def __repr__(self):
+        return self.task_description
+
+
 class ToDoList:
 
-    def __init__(self):
-        self.task_list = {}
+    def __init__(self, db_name='todo.db'):
+        self.engine = create_engine(f'sqlite:///{db_name}?check_same_thread=False')
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
 
-    def add(self, task_name, day='today'):
-        if day in self.task_list:
-            self.task_list[day].append(task_name)
-        else:
-            self.task_list[day] = [task_name]
+    def add(self, task_desk):
+        new_row = Table(task=task_desk)
+        self.session.add(new_row)
+        self.session.commit()
 
     def show_today(self):
-        if 'today' in self.task_list:
-            print('Today:')
-            for number, task in enumerate(self.task_list['today'], 1):
-                print(f'{number}) {task}')
+        rows = self.session.query(Table).all()
+        print('Today:')
+        if len(rows) == 0:
+            print('Nothing to do!')
         else:
-            print('Today you don\'t have tasks')
+            for number, item in enumerate(rows, 1):
+                print(f'{number}) {item.task}')
 
 
 tasks = ToDoList()
-tasks.add('Do yoga')
-tasks.add('Make breakfast')
-tasks.add('Learn basics of SQL')
-tasks.add('Learn what is ORM')
-tasks.show_today()
+while True:
+    print('1) Today\'s tasks')
+    print('2) Add task')
+    print('0) Exit')
+    user_command = input()
+    if '2' == user_command:
+        task = input('Enter task\n')
+        tasks.add(task)
+    elif '1' == user_command:
+        tasks.show_today()
+    elif '0' == user_command:
+        print('Bye!')
+        break
