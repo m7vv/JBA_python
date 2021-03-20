af949d658f3ef35b298aa5880fbe509c0a6856a9
mvv <mvv7mvv@gmail.com>
2021-03-20 20:16:33 +0200

    stage 3

--- SmartCalculator/calculator.py
+++ SmartCalculator/calculator.py
@@ -1,9 +1,11 @@
 while True:
     user_input = input().split()
-    if len(user_input) == 2:
-        print(int(user_input[0]) + int(user_input[1]))
+    if len(user_input) >= 2:
+        print(sum((int(number) for number in user_input)))
     if len(user_input) == 1:
         if user_input[0] == r'/exit':
             print('Bye!')
             break
+        if user_input[0] == r'/help':
+            print('The program calculates the sum of numbers')
         print(user_input[0])
